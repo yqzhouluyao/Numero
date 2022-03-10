c517d12b20118da37e41d056f34c8fef6309eb4f
zhouluyao <437001178@qq.com>
2022-03-10 11:44:12 +0800

    上传.travis.yml

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -27,3 +27,24 @@
 /// THE SOFTWARE.
 
 
+import XCTest
+@testable import Numero
+
+class ConverterTests: XCTestCase {
+  var converter: Converter!
+
+  override func setUp() {
+    super.setUp()
+    converter = Converter()
+  }
+
+  override func tearDown() {
+    converter = nil
+    super.tearDown()
+  }
+
+  func testConversionForOne() {
+    let result = converter.convert(1)
+    XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+  }
+}
